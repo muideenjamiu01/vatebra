eb3b02399d054a92f4eb766c16b803b3ebb882bf
EcJamiu <mmuideen@econnectnp.com>
2023-06-09 10:15:34 +0100

    updates

--- src/pages/countryCatalog.jsx
+++ src/pages/countryCatalog.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
-import styled from "styled-components";
-import { useQuery, gql } from "@apollo/client";
+// import styled from "styled-components";
+// import { useQuery, gql } from "@apollo/client";
 
 // const GET_COUNTRIES = gql`
 //   query {
